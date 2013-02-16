19a5449fb66b6f4e7b439ca2ec3edeb35f9002ad
Thomas Peklak <thomas.peklak@gmail.com>
2013-02-16 22:53:12 +0100

    add example of response middleware

---
+++ connect/05.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var connect = require("connect");
+var server  = connect.createServer();
+
+server.use(function (req, res, next) {
+    var end = res.end;
+    res.end = function (data) {
+        data = data.replace("world", "universe");
+        end.call(res, data);
+    };
+
+    next();
+});
+
+server.use(function (req, res) {
+    res.end("Hello world!");
+});
+
+server.listen(3003);
