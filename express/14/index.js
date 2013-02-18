70550789b86f6e96ee0407c01209c35e2e8c0bc3
Thomas Peklak <thomas.peklak@gmail.com>
2013-02-18 22:23:27 +0100

    add signed cookies to example

--- express/14/index.js
+++ express/14/index.js
@@ -7,11 +7,19 @@ app.use(express.cookieParser("My holy secret."));
 
 app.get("/set/:name/:value", function (req, res) {
     res.cookie(req.params.name, req.params.value, { httpOnly: true, maxAge: 1000 * 60 * 5 });
-    res.send("cookie stored. See it @ <a href='/get/" + req.params.name + "'>this url</a>");
+    res.send("cookie stored. See it @ <a href='/get'>this url</a>");
+});
+
+app.get("/signed/:name/:value", function (req, res) {
+    res.cookie(req.params.name, req.params.value, { maxAge: 1000 * 60 * 5, signed: true });
+    res.send("cookie stored. See it @ <a href='/get'>this url</a>");
 });
 
 app.get("/get", function (req, res) {
-    res.json(req.cookies);
+    res.json({
+        cookies: req.cookies,
+        signedCookies: req.signedCookies
+    });
 });
 
 app.get("/clear/:name", function (req, res) {
