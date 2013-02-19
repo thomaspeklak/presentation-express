d68cdbe4984b8aac8e51156bc94f29b9ca321ef1
Thomas Peklak <thomas.peklak@gmail.com>
2013-02-19 17:44:22 +0100

    add middleware example with simple photo browser

---
+++ express/17/index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var express = require("express");
+var app = express();
+
+
+app.use(express.basicAuth("admin", "secret"));
+app.use(express.directory(__dirname + "/photos", {icons: true}));
+app.use(express.static(__dirname + "/photos"));
+
+app.listen(3003);
